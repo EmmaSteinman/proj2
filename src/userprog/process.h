4b0b98e369976117539f3ddbb4840c2ee32d4190
Emma Steinman <33524355+EmmaSteinman@users.noreply.github.com>
2018-10-25 11:17:28 -0400

    process.h comments

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -5,25 +5,25 @@
 typedef int pid_t;
 
 struct process {
-  pid_t pid;
-  pid_t parent_pid;
-  struct list child_list;
-  struct list_elem procelem;
-  struct lock child_lock;
+  pid_t pid;                      //process identifier
+  pid_t parent_pid;               //pid of parent
+  struct list child_list;         //all of process' children
+  struct list_elem procelem;      
+  struct lock child_lock;         //allows for synchronization
 };
 
 struct child {
-  pid_t pid;
-  pid_t parent_pid;
-  int exit_status;
-  struct semaphore child_sema;
+  pid_t pid;                      //process identifier
+  pid_t parent_pid;               //pid of parent
+  int exit_status;                //exit status (for use of parent)
+  struct semaphore child_sema;    //allows for synchronization
   struct list_elem childelem;
-  struct semaphore load_done_sema;
+  struct semaphore load_done_sema;  //synchronization due to loading
   bool load_success;
 };
 
-struct list process_list;
-struct lock process_list_lock;
+struct list process_list;             //holding all current processes
+struct lock process_list_lock;        //allows for synchronization
 
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
